772f7edd46a4b5c2da2ec1713b2a6ee7cf5c4bc3
Nicholas Haase <nicholashaase@Elizabeth-Scotts-MacBook-Pro.local>
2017-06-20 13:22:04 -0700

    Initial Commit

---
+++ Project1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Nicholas Haase on 6/20/17.
+//  Copyright © 2017 Nicholas Haase. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
